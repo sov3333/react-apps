acfe9fbed467b9050d8a5f72f57b2f21bbe62381
SOV3333 <8282076+sov3333@users.noreply.github.com>
2022-12-27 15:55:30 +0800

    setup style.js

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,8 +1,13 @@
+import styles from './style';
 import React from 'react';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
         Navbar
+      </div>
+    </div>
   </div>
 );
 
