7b0e3c3542e002c62f0aac62d428630254472e96
SOV3333 <8282076+sov3333@users.noreply.github.com>
2022-12-27 15:58:22 +0800

    setup remaining sections on App.jsx

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -8,6 +8,26 @@ const App = () => (
         Navbar
       </div>
     </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonials
+        Clients
+        CTA
+        Footer
+      </div>
+    </div>
+
   </div>
 );
 
